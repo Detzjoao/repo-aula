1a62736afe680bc6301b701f27db9dba5eb22834
detzjoao <detz.jao@gmail.com>
2026-03-20 20:26:24 -0300

    adicionando funções

--- funcoes.php
+++ funcoes.php
@@ -1,3 +1,19 @@
 <?php
     echo "Vai Corinthians!";
+
+    function soma($num1,$num2){
+        return ($num1 + $num2);
+    }
+
+    function subtração($num1, $num2){
+        return ($num1 - $num2);
+    }
+
+    function multiplicação($num1, $num2){
+        return ($num1 * $num2);
+    }
+
+    function divisão($num1, $num2){
+        return ($num1 / $num2);
+    }
 ?>
